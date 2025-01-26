20d42f395a98ccde7cb53e2087f3617ffd7a2372
Dmitriy Paunin <d.m.paunin@gmail.com>
2025-01-26 18:52:41 +0800

    Add modules

--- app/root.tsx
+++ app/root.tsx
@@ -20,11 +20,9 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  {
-    rel: "icon",
-    type: "image/png",
-    href: "/images/favicon.png",
-  },
+  { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+  { rel: "icon", type: "image/png", href: "/favicon.png" },
+  { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
 ];
 
 export const meta: MetaFunction = () => {
